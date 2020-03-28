4d97d7d346b503998f4eeeb4c17f41ffc72ae96c
libohao666 <2052658718@qq.com>
2020-03-28 14:46:45 +0800

    20200328_14:46:20

--- server/server.c
+++ server/server.c
@@ -1,15 +1,15 @@
 #include "head.h"
 #include "tcp_server.h"
 
-int main(int argc, cahr **argv) {
+int main(int argc, char **argv) {
     int port, server_listen;
     
     if (argc != 2) {
-        frprintf(stderr, "Usage : %s port! \n", argv[0]);
+        fprintf(stderr, "Usage : %s port! \n", argv[0]);
         return 1;//参数错误
     }
     
-    prot = atoi(argv[1]);
+    port = atoi(argv[1]);
     
     if ((server_listen = socket_create(port)) < 0) {
         perror("socket_create");
