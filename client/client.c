9367445f6fc3f89352a36e0b0459f1edf31b52d1
libohao666 <2052658718@qq.com>
2020-03-28 14:55:40 +0800

    20200328_14:55:20

---
+++ client/client.c
@@ -0,0 +1,17 @@
+#include "head.h"
+#include "tcp_client.h"
+
+int main(int argc, char **argv) {
+    int sockfd;
+    if (argc != 3) {
+        fprintf(stderr, "Usage: %s ip port! \n", argv[0]);
+        return 1;//参数错误
+    }
+
+    if ((sockfd = socket_connect(argv[1], atoi(argv[2]))) < 0) {
+        perror("socket_connect");
+        return 2;
+    }
+
+    return 0;
+}
